86c7e04a0d62730f946c81b092a58a864470e241
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-04-27 00:05:51 +0600

    init: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
+  
+   if(typeof sampleActivity !== "string") {
+    return false;
+  }
+  
+     if(sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  
+       if(sampleActivity < 0) {
+    return false;
+  }
+  
+  k= 0.693/HALF_LIFE_PERIOD;
+  t = Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity));
+  z = Math.ceil(t/k);
+  //console.log('z:' + z);
+  return z;
 }
 
 module.exports = {
