87dde99d48ff56c43a26c2584c2f255dd8bc5393
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-04-27 00:13:50 +0600

    fix: param arr

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(arr) {
   
   
   if(!(arr instanceof Array)) {throw new Error()}
