feb44bbf65fee40f9a5ca47a226f31156862b22c
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-04-27 00:11:38 +0600

    init: transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,8 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  
+  
+  if(!(arr instanceof Array)) {throw new Error()}
+  
+   var narr = [];
+   
+if (arr.length === 0) return arr;
+  
+   for (var i = 0; i < arr.length; i +=1)
+   {
+    if (arr[i] === "--discard-prev")
+    {
+      if(0 <= i-1 && arr[i-2] !='--discard-next')
+      narr.pop();
+      //console.log('t: '+ i);  
+    }
+     else if (arr[i] === "--discard-next")
+     {
+       if(i+1 < arr.length)
+       i++;
+     }
+     else if (arr[i] === "--double-prev")
+     {
+     	 if(i-1 >= 0 && (arr[i - 2] !== '--discard-next'))
+           narr.push(arr[i - 1]);
+     }
+     else if (arr[i] === "--double-next")
+     {
+       if(i+1 < arr.length && !(arr[i+1] instanceof String))
+     	 narr.push(arr[i + 1]);
+     }
+     else  
+     {
+      narr.push(arr[i]);
+     }
+   }
+   return narr;emove line with error and write your code here
 }
 
 module.exports = {
