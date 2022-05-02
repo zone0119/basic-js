9909d3551b0944ea6aae8152303afe374e7c5aee
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-05-02 22:57:15 +0600

    fix: function param

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats( matrix ) {
  
   
             var counter=0;
